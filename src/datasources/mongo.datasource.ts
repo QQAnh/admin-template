be0f6df4492675181540f473fb0283afefc368b9
Hoàng Quỳnh Anh <anhhq@Hoangs-MacBook-Pro.local>
2024-03-26 14:29:39 +0700

    add datasouce

--- src/datasources/mongo.datasource.ts
+++ src/datasources/mongo.datasource.ts
@@ -5,7 +5,7 @@ const config = {
   name: 'Mongo',
   connector: 'mongodb',
   host: process.env.DB_HOST ?? 'localhost',
-  port: process.env.DB_PORT ?? '55000',
+  port: process.env.DB_PORT ?? '27017',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD ?? '',
   database: process.env.DB_DATABASE ?? '',
